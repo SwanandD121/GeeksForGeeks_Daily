3dd2c8a5e4c9729d880ca082c756490826c26b35
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-03-07 23:06:30 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Non-Repeating Element/non-repeating-element.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		while(testcases-- > 0){
+		    
+		    int n = Integer.parseInt(br.readLine());
+		    int arr[] = new int[n];
+		    
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split(" ");
+		    
+		    for(int index = 0;index < n; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Check obj = new Check();
+		    
+		    System.out.println(obj.firstNonRepeating(arr,n));
+		        
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Check{
+    
+    public int firstNonRepeating(int arr[], int n) 
+    { 
+        // Complete the function
+        HashMap<Integer, Integer> ans = new HashMap<>();
+        
+        for(int a : arr){
+            ans.put(a, ans.getOrDefault(a, 0) + 1);
+        }
+        
+        for(int a : arr){
+            if(ans.get(a) == 1){
+                return a;
+            }
+        }
+        return 0;
+        
+    }  
+    
+}
+
