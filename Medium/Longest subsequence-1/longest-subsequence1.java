2b096e370f8e635f15ae72b44873fc502c317df6
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-05-27 23:04:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest subsequence-1/longest-subsequence1.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] a = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            int res = obj.longestSubseq(n, a);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int longestSubseq(int n, int[] a) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            map.put(a[i], 1 + Math.max(map.getOrDefault(a[i] - 1, 0), map.getOrDefault(a[i] + 1, 0)));
+            ans = Math.max(ans, map.get(a[i]));
+        }
+        return ans;
+    }
+}
