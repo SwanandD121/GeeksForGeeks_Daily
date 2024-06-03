82b006099c5c7c651206a93569ff2ddbed65b5b3
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-06-03 15:25:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Ceil The Floor/ceil-the-floor.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Pair ans = new Solve().getFloorAndCeil(arr, n, x);
+            System.out.println(ans.floor + " " + ans.ceil);
+        }
+    }
+}
+
+class Pair {
+    int floor, ceil;
+
+    Pair() {
+        this.floor = 0;
+        this.ceil = 0;
+    }
+
+    Pair(int floor, int ceil) {
+        this.floor = floor;
+        this.ceil = ceil;
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solve {
+    Pair getFloorAndCeil(int[] arr, int n, int x) {
+        // code here
+        Arrays.sort(arr);
+        
+        
+        // for floor
+        int i = 0;
+        int j = n-1;
+        int floor = -1;
+        while(i <= j){
+            int mid = i + (j-i)/2;
+            if(arr[mid] <= x){
+                floor = arr[mid];
+                i = mid+1;
+            } else {
+                j = mid-1;
+            }
+        }
+        
+        // for ceil
+        int y = 0;
+        int z = n-1;
+        int ceil = -1;
+        while(y <= z){
+            int mid = y + (z-y)/2;
+            if(arr[mid] >= x){
+                ceil = arr[mid];
+                z = mid-1;
+            } else {
+                y = mid+1;
+            }
+        }
+        
+        Pair p1 = new Pair(floor, ceil);
+        
+        return p1;
+    }
+}
+
