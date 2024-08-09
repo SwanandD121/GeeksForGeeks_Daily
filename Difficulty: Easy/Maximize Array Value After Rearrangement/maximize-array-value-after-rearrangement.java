1361d41d6229a94ca79bf787a96a2454455a3908
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-08-09 10:45:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximize Array Value After Rearrangement/maximize-array-value-after-rearrangement.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            // int key = Integer.parseInt(in.readLine().trim());
+
+            out.println(new Solution().Maximize(arr));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // static final int MOD = 1000000007;
+
+    int Maximize(int arr[]) {
+        // Complete the function
+        Arrays.sort(arr);
+        long sum = 0;
+        int mod = (int)1e9 + 7;
+        for (int i = 0; i < arr.length; i++) {
+            sum += (long)i * arr[i];
+            sum %= mod;
+        }
+        
+        return (int) sum;
+    }
+}
