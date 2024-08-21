f7634b07f16d8052b48b3bf0398e770e558d4fb2
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-08-21 19:48:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t  =Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String st[] = read.readLine().trim().split(" ");
+            int N = Integer.parseInt(st[0]);
+            int M = Integer.parseInt(st[1]);
+            
+            int arr1[] = new int[N];
+            int arr2[] = new int[M];
+            
+            st = read.readLine().trim().split(" ");
+            for(int i = 0; i < N; i++)
+              arr1[i] = Integer.parseInt(st[i]);
+            
+            st = read.readLine().trim().split(" ");  
+            for(int i = 0; i< M; i++)
+              arr2[i] = Integer.parseInt(st[i]);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(arr1, arr2, N, M);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println(); 
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//arr1,arr2 : the arrays
+// n, m: size of arrays
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        // add your code here
+        ArrayList<Integer> comb = new ArrayList<>();
+        for(int a : arr1) comb.add(a);
+        for(int a : arr2) comb.add(a);
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int a : comb) map.put(a, map.getOrDefault(a, 0) + 1);
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        for(HashMap.Entry<Integer, Integer> set : map.entrySet()) res.add(set.getKey());
+        Collections.sort(res);
+        return res;
+    }
+}
+
+
+
