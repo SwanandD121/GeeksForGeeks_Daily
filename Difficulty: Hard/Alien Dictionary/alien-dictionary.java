2b1956900abed3bc0e52dafe0dd79e0ad93eed2a
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-08-22 09:45:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.java
@@ -0,0 +1,138 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = Integer.parseInt(sc.next());
+		while(t-- > 0)
+		{
+		    int n = Integer.parseInt(sc.next());
+		    int k = Integer.parseInt(sc.next());
+		    
+		    String[] words = new String[n];
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        words[i] = sc.next();
+		    }
+		    
+		    Solution ob = new Solution();
+		  //  System.out.println(T.findOrder(words,k));
+		    String order = ob.findOrder(words,n,k);
+		    if(order.length() == 0){
+		        System.out.println(0);
+		        continue;
+		    }
+		    if(order.length() != k){
+		        System.out.println("INCOMPLETE");
+		        return;
+		    }
+		    String temp[] = new String[n];
+		    for(int i=0;i<n;i++)
+		        temp[i] = words[i];
+		    
+		    Arrays.sort(temp, new Comparator<String>(){
+		    
+		      @Override
+                public int compare(String a, String b) {
+                    int index1 = 0;
+                    int index2 = 0;
+                    for(int i = 0; i < Math.min(a.length(), b.length()) 
+                                        && index1 == index2; i++) {
+                        index1 = order.indexOf(a.charAt(i));
+                        index2 = order.indexOf(b.charAt(i));
+                    }
+                
+                    if(index1 == index2 && a.length() != b.length()) 
+                    {
+                        if(a.length() < b.length())
+                            return -1;
+                        else
+                            return 1;
+                    }
+                
+                    if(index1 < index2)
+                        return -1;
+                    else
+                        return 1;
+                        
+                }
+		    });
+		    
+		    int flag = 1;
+		    for(int i=0;i<n;i++)
+		    {
+		        if(!words[i].equals(temp[i]))
+	            {
+	                flag = 0;
+	                break;
+	            }
+		    }
+		    
+		    System.out.println(flag);
+		}
+	}
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public String findOrder(String[] dict, int N, int K) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] indeg = new int[K];
+        
+        for (int i = 0; i < K; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int i = 0; i < N - 1; i++) {
+            String s1 = dict[i];
+            String s2 = dict[i + 1];
+            int len = Math.min(s1.length(), s2.length());
+            for (int j = 0; j < len; j++) {
+                if (s1.charAt(j) != s2.charAt(j)) {
+                    int u = s1.charAt(j) - 'a';
+                    int v = s2.charAt(j) - 'a';
+                    adj.get(u).add(v);
+                    indeg[v]++;
+                    break;
+                }
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < K; i++) {
+            if (indeg[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            ans.append((char) (node + 'a'));
+            for (int neighbor : adj.get(node)) {
+                indeg[neighbor]--;
+                if (indeg[neighbor] == 0) {
+                    q.offer(neighbor);
+                }
+            }
+        }
+
+        if (ans.length() < K) {
+            return ""; 
+        }
+
+        return ans.toString();
+    }
+}
