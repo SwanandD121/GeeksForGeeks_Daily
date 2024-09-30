7110694b6fb6d862d0a31ebaabb039aaabf578fd
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-09-30 22:51:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -110,21 +110,31 @@ class Node
 class Solution {
     // Function to return a list of integers denoting the node
     // values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1, Node root2) {
+        // Write your code here
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        inorderTraversal(root1, list1);
+        inorderTraversal(root2, list2);
+
+        return mergeSortedLists(list1, list2);
+    }
     private void inorderTraversal(Node root, List<Integer> result) {
-        if (root == null) {
-            return;
-        }
+        if (root == null) return;
+        
         inorderTraversal(root.left, result);
         result.add(root.data);
         inorderTraversal(root.right, result);
     }
 
+    // Function to merge two sorted lists
     private List<Integer> mergeSortedLists(List<Integer> list1, List<Integer> list2) {
         List<Integer> mergedList = new ArrayList<>();
         int i = 0, j = 0;
         
         while (i < list1.size() && j < list2.size()) {
-            if (list1.get(i) < list2.get(j)) {
+            if (list1.get(i) <= list2.get(j)) {
                 mergedList.add(list1.get(i));
                 i++;
             } else {
@@ -144,15 +154,5 @@ class Solution {
         }
 
         return mergedList;
-    }
-
-    public List<Integer> merge(Node root1, Node root2) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-
-        inorderTraversal(root1, list1);
-        inorderTraversal(root2, list2);
-
-        return mergeSortedLists(list1, list2);
     }
 }
