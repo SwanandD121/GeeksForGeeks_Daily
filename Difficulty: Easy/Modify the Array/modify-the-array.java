4cbecca1810285687e528c92591a0c46578adf5d
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-10-24 11:00:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Modify the Array/modify-the-array.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] arr = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) arr[idx++] = i;
+
+            v = new Solution().modifyAndRearrangeArr(arr);
+
+            for (int i = 0; i < v.size(); i++) System.out.print(v.get(i) + " ");
+
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static ArrayList<Integer> modifyAndRearrangeArr(int arr[]) {
+        // Complete the function
+        int n = arr.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != 0 && arr[i] == arr[i + 1]) {
+                arr[i] *= 2;
+                arr[i + 1] = 0;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                list.add(arr[i]);
+            }
+        }
+        
+        while (list.size() < n) {
+            list.add(0);
+        }
+        
+        return list;
+    }
+}
