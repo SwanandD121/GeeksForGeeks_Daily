920924d68e84b903e154ab26804ed017a2fcbccc
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-12-10 23:54:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Non-overlapping Intervals/nonoverlapping-intervals.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[][] = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                a[i][0] = sc.nextInt();
+                a[i][1] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            int ans = obj.minRemoval(a);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int minRemoval(int intervals[][]) {
+        // code here
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
+        int last = 0,ans=0,n=intervals.length;
+        for(int i=1;i<n;i++){
+            if(intervals[i][0] <intervals[last][1])ans++;
+            else last=i;
+        }
+        return ans;
+    }
+}
