eef3235c2b040fd2da9165d61097a92d1af798e9
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2024-12-22 14:14:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Row-Column sorted matrix/search-in-a-row-column-sorted-matrix.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class gfg {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            int mat[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) mat[i][j] = sc.nextInt();
+            }
+
+            int x = sc.nextInt();
+
+            if (new Solution().matSearch(mat, x))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static boolean matSearch(int mat[][], int x) {
+        // your code here
+        int row = 0;
+        int col = mat[0].length - 1;
+        while(row < mat.length && col >= 0) {
+            if(mat[row][col] == x) {
+                return true;
+            } else if(mat[row][col] < x) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
