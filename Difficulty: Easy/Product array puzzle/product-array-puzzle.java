3542dedeeecba77848bc260a12a58374ede5b74f
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2025-01-17 14:28:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.java
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -6,22 +6,24 @@ import java.util.*;
 
 class GFG {
     public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         while (t > 0) {
-            int n = sc.nextInt();
-            int[] array = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
             for (int i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
+                arr[i] = Integer.parseInt(inputLine[i]);
             }
             Solution ob = new Solution();
-            long[] ans = new long[n];
-            ans = ob.productExceptSelf(array);
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
 
             for (int i = 0; i < n; i++) {
                 System.out.print(ans[i] + " ");
             }
             System.out.println();
+            System.out.println("~");
             t--;
         }
     }
@@ -31,28 +33,21 @@ class GFG {
 
 
 // User function Template for Java
-
 class Solution {
-    public static long[] productExceptSelf(int nums[]) {
+    public static int[] productExceptSelf(int arr[]) {
         // code here
-        int n = nums.length;
-        long res[] = new long[n];
-        
-        for(int i = 0; i < n; i++){
-            res[i] = 1;
-        }
-        
-        long left = 1;
-        long right = 1;
+        int n = arr.length;
+        int res[] = new int[n];
+        for(int i = 0; i < n; i++) res[i] = 1;
+        int left = 1, right = 1;
         
         for(int i = 0; i < n; i++){
             res[i] = left;
-            left *= nums[i];
+            left *= arr[i];
         }
-        
         for(int i = n-1; i >= 0; i--){
             res[i] *= right;
-            right *= nums[i];
+            right *= arr[i];
         }
         return res;
     }
