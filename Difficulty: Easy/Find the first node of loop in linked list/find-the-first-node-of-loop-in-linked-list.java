3f478ec08b520c96c27f4b175a442c05427efaf2
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2025-01-25 10:42:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.java
+++ Difficulty: Easy/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.java
@@ -1,103 +1,70 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class Node
-{
+class Node {
     int data;
     Node next;
-    
-    Node(int x)
-    {
+
+    Node(int x) {
         data = x;
         next = null;
     }
 }
 
-class GFG
-{
-    static class FastReader{ 
-        BufferedReader br; 
-        StringTokenizer st; 
-  
-        public FastReader(){ 
-            br = new BufferedReader(new InputStreamReader(System.in)); 
-        } 
-  
-        String next(){ 
-            while (st == null || !st.hasMoreElements()){ 
-                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
-            } 
-            return st.nextToken(); 
-        } 
-  
-        String nextLine(){ 
-            String str = ""; 
-            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
-            return str; 
-        } 
-
-        Integer nextInt(){
-            return Integer.parseInt(next());
+public class LinkedList {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
         }
-
-        Long nextLong(){
-            return Long.parseLong(next());
-        }
+        System.out.println();
     }
-    
-    public static void makeLoop(Node head, Node tail, int x){
+
+    public static void makeLoop(Node head, Node tail, int x) {
         if (x == 0) return;
-        
+
         Node curr = head;
-        for(int i=1; i<x; i++)
-            curr = curr.next;
-        
+        for (int i = 1; i < x; i++) curr = curr.next;
+
         tail.next = curr;
     }
-    
-    public static void main (String[] args){
-        FastReader sc = new FastReader();
-        PrintWriter out = new PrintWriter(System.out);
-        int t = sc.nextInt();
-        
-        while(t--> 0)
-        {
-            int n = sc.nextInt();
-            
-            int num = sc.nextInt();
-            Node head = new Node(num);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
             Node tail = head;
-            
-            for(int i=0; i<n-1; i++)
-            {
-                num = sc.nextInt();
-                tail.next = new Node(num);
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
                 tail = tail.next;
             }
-            
-            int pos = sc.nextInt();
-            makeLoop(head, tail, pos);
-            Solution x = new Solution();
-            out.println(x.findFirstNode(head));
+            makeLoop(head, tail, k);
+            Solution ob = new Solution();
+            Node ans = ob.findFirstNode(head);
+            System.out.println(ans == null ? -1 : ans.data);
+            System.out.println("~");
         }
-        out.flush();
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
-
+// User function Template for Java
 
 /* class Node
 {
     int data;
     Node next;
-    
+
     Node(int x)
     {
         data = x;
@@ -106,31 +73,24 @@ class GFG
 }*/
 
 class Solution {
-    //Function to find first node if the linked list has a loop.
-    public static int findFirstNode(Node head){
-        //code here
-        if(head == null || head.next == null) return -1;
-        
-        // check cycle
+    public static Node findFirstNode(Node head) {
+        // code here
         Node slow = head;
         Node fast = head;
-        boolean cycle = false;
         
-        while(fast != null && fast.next != null){
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(fast == slow){
-                cycle = true;
-                break;
+            
+            if(slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
         }
-        if(cycle == false) return -1;
-        
-        slow = head;
-        while(fast != slow){
-            slow = slow.next;
-            fast = fast.next;
-        }
-        return slow.data;
+        return new Node(-1);
     }
 }
