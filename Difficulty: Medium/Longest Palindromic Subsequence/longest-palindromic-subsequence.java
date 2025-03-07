deb29eb88b47ef23e5665c5bcacb08ef9bc3f716
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2025-03-07 11:11:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.longestPalinSubseq(s));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int longestPalinSubseq(String s) {
+        // code here
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) ? 2 + dp[i + 1][j - 1] : Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
