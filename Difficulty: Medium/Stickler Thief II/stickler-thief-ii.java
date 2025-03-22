d5874035dc5ea7e4699361dd18c151f0137428e9
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2025-03-22 19:03:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief II/stickler-thief-ii.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int n = s.length;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int[] arr = IntArray.input(br);
+
+            Solution obj = new Solution();
+            int res = obj.maxValue(arr);
+
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int maxValue(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return arr[0];
+        return Math.max(rob(arr, 0, n - 2), rob(arr, 1, n - 1));
+    }
+
+    int rob(int[] arr, int start, int end) {
+        int prev2 = 0, prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int curr = Math.max(prev1, prev2 + arr[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
