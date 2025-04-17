7c2e938e263c3c60fca0079f44d3a2ec48fbc71e
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2025-04-18 05:02:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Trie/implement-trie.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Trie {
+
+    private class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd = false;
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+            curr = curr.children[idx];
+        }
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (curr.children[idx] == null) return false;
+            curr = curr.children[idx];
+        }
+        return curr.isEnd;
+    }
+
+    public boolean isPrefix(String word) {
+        TrieNode curr = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (curr.children[idx] == null) return false;
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+public class GfG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            int q = sc.nextInt();
+            sc.nextLine();
+            List<Boolean> ans = new ArrayList<>();
+            Trie ob = new Trie();
+
+            for (int i = 0; i < q; i++) {
+                int x = sc.nextInt();
+                String s = sc.next();
+                if (i != q - 1) sc.nextLine();
+
+                if (x == 1) {
+                    ob.insert(s);
+                } else if (x == 2) {
+                    ans.add(ob.search(s));
+                } else if (x == 3) {
+                    ans.add(ob.isPrefix(s));
+                }
+            }
+
+            for (Boolean result : ans) {
+                System.out.print(result + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
