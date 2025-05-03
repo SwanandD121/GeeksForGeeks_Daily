bc638ff8fa2320220908f7ca8689c60d2ea14311
Swanand Deshpande <117746995+SwanandD121@users.noreply.github.com>
2025-05-03 14:55:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prime List/prime-list.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Node {
+    Node next;
+    int val;
+
+    public Node(int data) {
+        val = data;
+        next = null;
+    }
+}
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t;
+        t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+
+            Node head, tail;
+            String s[] = in.readLine().trim().split(" ");
+            int num = Integer.parseInt(s[0]);
+            head = new Node(num);
+            tail = head;
+            for (int i = 1; i < s.length; i++) {
+                num = Integer.parseInt(s[i]);
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node temp = ob.primeList(head);
+            while (temp != null) {
+                out.print(temp.val + " ");
+                temp = temp.next;
+            }
+            out.println();
+            out.println("~");
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+class Node{
+    Node next;
+    int val;
+    public Node(int data){
+        val=data;
+        next=null;
+    }
+}
+*/
+
+class Solution {
+    boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    int nearestPrime(int n) {
+        if (n <= 2) return 2;
+        int lower = n, upper = n;
+        while (lower >= 2 && !isPrime(lower)) lower--;
+        while (!isPrime(upper)) upper++;
+        if (n - lower <= upper - n) return lower;
+        return upper;
+    }
+
+    Node primeList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            temp.val = nearestPrime(temp.val);
+            temp = temp.next;
+        }
+        return head;
+    }
+}
